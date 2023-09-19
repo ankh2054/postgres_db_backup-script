603b86d0c5e552d91dadf96aad805bfe5898320d
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2023-09-19 14:33:17 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 import preprocessing
 s3 = {
     "endpoint_url": "https://s3.eu-central-1.wasabisys.com",
-    "aws_access_key_id": "1J7XQIHHZ6TTMGDVEL1G",
-    "aws_secret_access_key": "2tS8gegOl0dkozbDYwU80Je2MChv0TEGztTiObPr",
+    "aws_access_key_id": "",
+    "aws_secret_access_key": "",
     "wasabi_bucket": "misst-backups"
 }
 core = {
